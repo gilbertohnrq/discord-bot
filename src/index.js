722a452512f95103dd4087c3198f487193e87c5b
gilbertohnrq <gilbertohnrq@gmail.com>
2020-08-14 22:15:33 -0300

    adding bot

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+require('dotenv').config();
+
+const client = new Discord.Client();
+
+const guildID = '122486807869915146';
+const channelID = '258392309282897921';
+
+client.once('ready', () => {
+  console.log('Bot is ready!');
+});
+
+client.on('message', async (msg) => {
+  if (msg.guild.id === guildID && msg.channel.id === channelID) {
+    if (msg.content.toLowerCase() === 'ricardo') {
+      await msg.channel.send('gay');
+    }
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
