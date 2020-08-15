00746c0c4c1336dec7fe3ea94fbde9588f3cd2a9
gilbertohnrq <gilbertohnrq@gmail.com>
2020-08-14 23:59:21 -0300

    changing bot to listen to all channels

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ const channelID = '258392309282897921';
 client.once('ready', () => {
   console.log('Bot is ready!');
 });
+//&& msg.channel.id === channelID adiciona a condição de channels
 try {
   client.on('message', async (msg) => {
-    if (msg.guild.id === guildID && msg.channel.id === channelID) {
+    if (msg.guild.id === guildID) {
       if (msg.content.toLowerCase() === 'ricardo') {
         await msg.channel.send('gay');
       }
