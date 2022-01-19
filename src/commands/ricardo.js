57c6d3ccf8b67fe8621155c794bfd83875f7bca5
Gilberto Henrique <gilbertohnrq@gmail.com>
2022-01-19 00:12:35 -0300

    update ricardo

--- src/commands/ricardo.js
+++ src/commands/ricardo.js
@@ -1,4 +1,12 @@
+var textArray = [
+  'é gay',
+  'maconheiro',
+  '=rage'
+];
+var randomIndex = Math.floor(Math.random() * textArray.length); 
+var randomText = textArray[randomIndex];
+
 module.exports = async (msg) => {
-  await msg.channel.send('é gay');
+  await msg.channel.send(randomText);
 };
 
